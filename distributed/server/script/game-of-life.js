cd929e27893539c6a0987de4632315e4ae1eced9
Ed Sumerfield <esumerfd@bitbashers.org>
2024-11-19 20:49:58 -0500

    Add render static board in skeleton

--- distributed/server/script/game-of-life.js
+++ distributed/server/script/game-of-life.js
@@ -1,4 +1,17 @@
 function lets_go() {
-  var board = document.getElementById("board")
+  const element = document.getElementById("board")
+  const board = new Board(element)
 
+  board.render();
+
+}
+
+class Board {
+  constructor(element) {
+    this.board = element
+  }
+
+  render() {
+    this.board.innerHTML = "<table><tr><td>_</td><td>_</td></tr></table>"
+  }
 }
