d88cd6f377bb04f4c5aa319614ac14866f505cb2
Ed Sumerfield <esumerfd@bitbashers.org>
2024-12-01 16:18:21 -0500

    Add web worker

--- distributed/browser/GameOfLife/src/app/board/board.component.ts
+++ distributed/browser/GameOfLife/src/app/board/board.component.ts
@@ -8,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class BoardComponent {
 
+  public ready() {
+    if (typeof Worker !== 'undefined') {
+      const worker = new Worker(new URL('../board_worker/board.worker', import.meta.url));
+      worker.onmessage = ({ data }) => {
+        console.log(data);
+      };
+      worker.postMessage('ready');
+    } else {
+      // Web workers are not supported in this environment.
+      // You should add a fallback so that your program still executes correctly.
+      console.log("oops")
+    }
+  }
 }
