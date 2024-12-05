9e8fa87a08c1331e0c34314a1fda7509d8cba4a1
Ed Sumerfield <esumerfd@bitbashers.org>
2024-12-04 21:12:56 -0500

    Add rendered board and distribute encoded board from worker

--- distributed/browser/GameOfLife/src/app/board/board.component.ts
+++ distributed/browser/GameOfLife/src/app/board/board.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
-import { WorkerFactory } from '../../lib/worker/workerFactory';
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { WorkerFactory } from '../../lib/worker/workerFactory'
 
 @Component({
   selector: 'app-board',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './board.component.html',
   styleUrl: './board.component.css'
 })
@@ -11,15 +12,12 @@ export class BoardComponent {
 
   worker: Worker
 
-  message: string
+  message: string = ""
+
+  rows: Array<Array<string>> = [Array.from("Please wait. Rendering.")]
 
   constructor() {
-
-    this.worker = WorkerFactory.board(data => {
-      this.message = data
-    });
-
-    this.message = ""
+    this.worker = WorkerFactory.board((data) => this.render(data));
   }
 
   public ready() {
@@ -28,5 +26,45 @@ export class BoardComponent {
 
   public status() {
     this.worker.postMessage('status');
+  }
+
+  public render(message: string) {
+    this.message = "Running"
+
+    let rows = this.parseBoard(message)
+
+    this.renderRows(rows)
+  }
+
+  parseBoard(encodedBoard: string): Array<Array<string>> {
+    let rows: Array<Array<string>> = []
+
+    let encodedRows = encodedBoard.split('\n')
+    encodedRows.forEach((encodedRow: string) => {
+      rows.push(Array.from(encodedRow))
+    })
+
+    return rows
+  }
+
+  renderRows(rows: Array<Array<string>>) {
+
+    this.rows = []
+    let renderRow: Array<string>
+    this.each_row(rows, (row: Array<string>) => {
+      renderRow = []
+      this.rows.push(renderRow)
+      this.each_cell(row, (cell: string) => {
+        renderRow.push(cell)
+      })
+    })
+  }
+
+  each_row(rows: Array<Array<string>>, renderRow: (row: Array<string>) => void) {
+    Array.from(rows).forEach(renderRow)
+  }
+
+  each_cell(row: Array<string>, renderCell: (cell: string) => void) {
+    Array.from(row).forEach((cell) => renderCell(cell) )
   }
 }
--- distributed/browser/GameOfLife/src/app/board_worker/board.worker.ts
+++ distributed/browser/GameOfLife/src/app/board_worker/board.worker.ts
@@ -8,3 +8,34 @@ addEventListener('message', ({ data }) => {
     postMessage(`status: unknown`);
   }
 });
+
+function initialize() {
+  setInterval(loop, 1000)
+}
+
+let board = [Array.from("__________"),Array.from("__________"),Array.from("__________"),Array.from("__________"),Array.from("__________"),Array.from("__________"),Array.from("__________"),Array.from("__________"),Array.from("__________"),Array.from("__________")]
+
+function loop() {
+  applyChange(board)
+
+  let encodedBoard = encodeBoard(board)
+
+  postMessage(encodedBoard)
+}
+
+function applyChange(board: Array<Array<string>>) {
+  let randomRow = Math.floor(Math.random() * 10);
+  let randomCell = Math.floor(Math.random() * 10);
+
+  board[randomRow][randomCell] = "X"
+}
+
+function encodeBoard(board: Array<Array<string>>): string {
+  let encodedBoard = ""
+  for (let row in board) {
+    encodedBoard += board[row].join("") + "\n"
+  }
+  return encodedBoard
+}
+
+initialize()
